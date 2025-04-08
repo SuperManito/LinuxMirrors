c5ff4bfc4f3240f2224cc24bf99ff37ab23e027d
Super Manito <68613938+SuperManito@users.noreply.github.com>
2025-04-09 02:59:14 +0800

    更新文档

--- docs/assets/js/component.js
+++ docs/assets/js/component.js
@@ -1,15 +1,4 @@
 // 使用 mkdocs-material 与第三方 JavaScript 库集成的方法
 document$.subscribe(function () {
-    // 重新初始化组件
     ComponentSystem.reinitializeAll()
-    // 延迟初始化以确保DOM完全渲染
-    setTimeout(() => {
-        ComponentSystem.initAll()
-    }, 300)
-})
-
-// 首次加载事件
-window.addEventListener('load', function () {
-    // 初始化所有组件
-    ComponentSystem.initAll()
 })
